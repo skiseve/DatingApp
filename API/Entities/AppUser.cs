ff232dc4fefca09fa02ca995bc8eb6edd18a4bbb
Samuel Kiseve <samuel@pathwaysinternational.com>
2022-03-19 19:13:41 +0300

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        // private int myVar;
+        // public int MyProperty
+        // {
+        //     get { return myVar; }
+        //     set { myVar = value; }
+        // }
+        
+    }
+}
